a0a5a1d3a6e840363f24aeb2c0b40fc0dc93a4a4
Anna <aprusik@tlen.pl>
2020-10-24 14:07:15 +0200

    some changes added

--- src/main/java/dao/PresentRentalsDao.java
+++ src/main/java/dao/PresentRentalsDao.java
@@ -12,6 +12,13 @@ public class PresentRentalsDao extends AbstractDao<PresentRentals> {
     public PresentRentalsDao() {
         super(PresentRentals.class);
     }
+    public List<PresentRentals> lateReturn (LocalDateTime plannedReturnDate, LocalDateTime returnDate) {
+        Session session = SessionProvider.getSession();
+        List<PresentRentals> records = session.createQuery("select from PresentRentals where returnDate-plannedReturnDate>0", PresentRentals.class)
+                .list();
+        session.close();
+        return records;
+    }
 
 
 }
