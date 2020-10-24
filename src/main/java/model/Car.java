8fe762f8065173fc925ead2d836dca4e8f4ed6c6
Anna <aprusik@tlen.pl>
2020-10-24 11:24:29 +0200

    new added files

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -3,6 +3,7 @@ package model;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "car")
@@ -25,6 +26,95 @@ public class Car {
     @Column
     private String registrationNumber;
 
+    @Column
+    private boolean isDamaged;
 
+    public Car() {
+    }
 
+    public Car(String brand, String model, String color, String registrationNumber, boolean isDamaged) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registrationNumber = registrationNumber;
+        this.isDamaged = isDamaged;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public boolean isDamaged() {
+        return isDamaged;
+    }
+
+    public void setDamaged(boolean damaged) {
+        isDamaged = damaged;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id &&
+                isDamaged == car.isDamaged &&
+                Objects.equals(brand, car.brand) &&
+                Objects.equals(model, car.model) &&
+                Objects.equals(color, car.color) &&
+                Objects.equals(registrationNumber, car.registrationNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, brand, model, color, registrationNumber, isDamaged);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", isDamaged=" + isDamaged +
+                '}';
+    }
 }
