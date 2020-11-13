d73a5a3dfbede226584c5733062573019720159b
Anna <aprusik@tlen.pl>
2020-11-13 20:39:51 +0100

    my trials with Swing

--- src/main/java/dao/CustomerDao.java
+++ src/main/java/dao/CustomerDao.java
@@ -1,10 +1,22 @@
 package dao;
 
+import database.SessionProvider;
+import model.Car;
 import model.Customer;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class CustomerDao extends AbstractDao<Customer> {
 
     public CustomerDao() {
         super(Customer.class);
+    }
+
+    public List<Customer> findAll() {
+        Session session = SessionProvider.getSession();
+        List<Customer> customerList = session.createQuery("from Customer",Customer.class).list();
+        session.close();
+        return customerList;
     }
 }
